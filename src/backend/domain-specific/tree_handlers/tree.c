bf82b850aa828cfb4054a9b4096debc25c5f0236
Roman Gomez Kiss <romgomez@itba.edu.ar>
2022-11-19 17:36:36 -0300

    Agregado de más funciones para legend. Posibilidad de cambiar a tipo avl o bst

--- src/backend/domain-specific/tree_handlers/tree.c
+++ src/backend/domain-specific/tree_handlers/tree.c
@@ -5,48 +5,20 @@
 #include "avl_tree.h"
 #include "rbt_tree.h"
 
-struct node* minValueNode(struct node* node) {
-    struct node* current = node;
+static int max(int a, int b);
+static void transformToBst(struct node* node);
+static struct node* transformToAvl(struct node* node) ;
+static int addToArray(struct node* node, int arr[], int i);
+int* transformTreeToArray(struct node* node, int* size);
 
-    while (current && current->left != NULL)
-        current = current->left;
-
-    return current;
-}
-
-void generateDot(struct node* node) {
-    if (node != NULL) {
-        if (node->found) {
-            printf("%d [color=\"green\"]", node->key);
-        }
-
-        if (node->left != NULL) {
-
-            printf("\t%d->%d;\n", node->key, node->left->key);
-            generateDot(node->left);
-        }
-        if (node->right != NULL) {
-
-            printf("\t%d->%d;\n", node->key, node->right->key);
-            generateDot(node->right);
-        }
-    }
-}
-
-void generateDotFile(struct node* node) {
-    printf("strict digraph{ \n");
-    generateDot(node);
-    printf("}");
-}
-
-struct node* insertFirstNode(struct node* node, int key, treeTypeEnum type){
+struct node* insertFirstNode(struct node* node, int key, treeTypeEnum type) {
     switch (type) {
         case BST:
             return insertBstNode(node, key);
         case AVL:
             return insertAvlNode(node, key);
         case RBT:
-            //return insertRbtNode(node, key);
+            // return insertRbtNode(node, key);
             return NULL;
         default:
             return NULL;
@@ -60,7 +32,7 @@ struct node* insertNode(struct node* node, int key) {
         case AVL:
             return insertAvlNode(node, key);
         case RBT:
-            //return insertRbtNode(node, key);
+            // return insertRbtNode(node, key);
             return NULL;
         default:
             return NULL;
@@ -78,7 +50,7 @@ struct node* deleteNode(struct node* node, int key) {
         case AVL:
             return deleteAvlNode(node, key);
         case RBT:
-            //return deleteRbtNode(node, key);
+            // return deleteRbtNode(node, key);
             return NULL;
         default:
             return NULL;
@@ -101,51 +73,163 @@ void findNode(struct node* node, int key) {
     }
 }
 
-void freeTree(struct node* node) {
-    if (node!=NULL) {
-		freeTree(node->left);
-		freeTree(node->right);
-		free(node);
-	}
+struct node* switchType(struct node* node, treeTypeEnum type) {
+    if (node->type == type) {
+        return node;
+    }
+
+    switch (type) {
+        case BST:
+            transformToBst(node);
+            return node;
+
+        case AVL:
+            return transformToAvl(node);
+
+        case RBT:
+
+            break;
+
+        default:
+            break;
+    }
 }
 
-static void transformToBst(struct node* node){
-    if (node!=NULL){
-        node->height=0;
-        node->par=NULL;
-        node->color=NONE;
-        node->type=BST;
+void generateDot(struct node* node) {
+    if (node != NULL) {
+        if (node->found) {
+            printf("%d [color=\"green\"]", node->key);
+        }
+
+        if (node->left != NULL) {
+
+            printf("\t%d->%d;\n", node->key, node->left->key);
+            generateDot(node->left);
+        }
+        if (node->right != NULL) {
+
+            printf("\t%d->%d;\n", node->key, node->right->key);
+            generateDot(node->right);
+        }
+    }
+}
+
+void freeTree(struct node* node) {
+    if (node != NULL) {
+        freeTree(node->left);
+        freeTree(node->right);
+        free(node);
+    }
+}
+
+struct node* minValueNode(struct node* node) {
+    struct node* current = node;
+
+    while (current && current->left != NULL)
+        current = current->left;
+
+    return current;
+}
+
+struct node* maxValueNode(struct node* node) {
+    struct node* current = node;
+
+    while (current && current->right != NULL)
+        current = current->right;
+
+    return current;
+}
+
+int nodeCount(struct node* node) {
+    int c = 1;
+
+    if (node == NULL)
+        return 0;
+    else {
+        c += nodeCount(node->left);
+        c += nodeCount(node->right);
+        return c;
+    }
+}
+
+void generateDotFile(struct node* node) {
+    printf("strict digraph{ \n");
+    generateDot(node);
+    printf("}");
+}
+
+int treeHeight(struct node* node) {
+    if (node == NULL)
+        return 0;
+
+    return 1 + max(treeHeight(node->left), treeHeight(node->right));
+}
+
+int isBalanced(struct node* root) {
+    int lh;
+    int rh;
+
+    if (root == NULL)
+        return 1;
+
+    lh = treeHeight(root->left);
+    rh = treeHeight(root->right);
+
+    if (abs(lh - rh) <= 1 && isBalanced(root->left) && isBalanced(root->right))
+        return 1;
+
+    return 0;
+}
+
+static int max(int a, int b) { return (a >= b) ? a : b; }
+
+static void transformToBst(struct node* node) {
+    if (node != NULL) {
+        node->height = 0;
+        node->par = NULL;
+        node->color = NONE;
+        node->type = BST;
         transformToBst(node->left);
         transformToBst(node->right);
     }
 }
 
-static struct node* transformToAvl(struct node* node, struct node* toRet){
-    //check como insertar recursivamente los nodos de un arbol a otro de otro tipo
+static int addToArray(struct node* node, int arr[], int i) {
+    if (node == NULL)
+        return i;
+
+    arr[i] = node->key;
+    i++;
+    if (node->left != NULL)
+        i = addToArray(node->left, arr, i);
+    if (node->right != NULL)
+        i = addToArray(node->right, arr, i);
+
+    return i;
 }
 
-struct node* switchType(struct node* node, treeTypeEnum type){
-    struct node* toRet = node;
+int* transformTreeToArray(struct node* node, int* size) {
+    int count = nodeCount(node);
+    int i = 0;
+    int* allNodes = malloc(count * sizeof(int));
+    i = addToArray(node, allNodes, i);
 
-    if(node->type==type){
-        return toRet;
+    *size = i;
+
+    return allNodes;
+}
+
+static struct node* transformToAvl(struct node* node) {
+    int i;
+    int* allNodes = transformTreeToArray(node, &i);
+    struct node* toRet = NULL;
+
+    for (int j = 0; j < i; j++) {
+        toRet = insertAvlNode(toRet, allNodes[j]);
     }
 
-    switch (type)
-    {
-    case BST:
-        transformToBst(node);
-        return toRet;
-    
-    case AVL:
-    //check how to do
-        return toRet;
+    free(allNodes);
+    freeTree(node);
 
-    case RBT:
-
-        break;
-    
-    default:
-        break;
-    }
+    return toRet;
 }
--- src/backend/domain-specific/tree_handlers/tree.h
+++ src/backend/domain-specific/tree_handlers/tree.h
@@ -32,9 +32,15 @@ struct node* insertFirstNode(struct node* node, int key, treeTypeEnum type);
 struct node* insertNode(struct node* node, int key);
 struct node* deleteNode(struct node* node, int key);
 void findNode(struct node* node, int key);
-struct node* minValueNode(struct node* node);
-void generateDot(struct node* node);
 struct node* switchType(struct node* node, treeTypeEnum type);
+void generateDot(struct node* node);
 void freeTree(struct node* node);
+
+//To be used in legend
+struct node* minValueNode(struct node* node);
+struct node* maxValueNode(struct node* node);
+int nodeCount(struct node* node);
+int treeHeight(struct node* node);
+int isBalanced(struct node* node);
 
 #endif
